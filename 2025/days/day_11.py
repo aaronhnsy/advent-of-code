0d2b90a8af54330620d6853647ad3140d5a20f9f
aaronhnsy <aaronhnsy@gmail.com>
2025-12-11 05:48:28 +0000

    add 2025 day 11

--- 2025/days/day_11.py
+++ 2025/days/day_11.py
@@ -1,17 +1,42 @@
+import functools
+
 from aoc import Day
 
 
-class Day11(Day[None]):
+class Day11(Day[dict[str, list[str]]]):
 
-    def parse(self):
-        return
+    def parse(self) -> dict[str, list[str]]:
+        return {
+            machine: outputs.lstrip().split(" ")
+            for machine, outputs in map(lambda line: line.split(":"), self.LINES)
+        }
 
-    def part_one(self):
-        return 0
+    def part_one(self) -> int:
+        @functools.cache
+        def check(machine: str, paths: int = 0) -> int:
+            return paths + (
+                1 if ("out" in self.input[machine])
+                else sum(map(
+                    lambda output: check(output, paths),
+                    self.input[machine]
+                ))
+            )
 
-    def part_two(self):
-        return 0
+        return check("you")
+
+    def part_two(self) -> int:
+        @functools.cache
+        def check(machine: str, paths: int = 0, dac: bool = False, fft: bool = False) -> int:
+            return paths + (
+                (1 if (dac and fft) else 0) if ("out" in self.input[machine])
+                else sum(map(
+                    lambda output: check(output, paths, dac or machine == "dac", fft or machine == "fft"),
+                    self.input[machine],
+                ))
+            )
+
+        return check("svr")
 
 
 day = Day11()
-day.time(day.part_one, day.part_two)
+day.run(day.part_one, day.part_two)
